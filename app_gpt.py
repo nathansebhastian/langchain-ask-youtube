ebce088ef46c7cae56ae93b3d553653f234b971f
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-08-09 14:04:16 +0700

    add sqlite3 binary for streamlit issue

--- app_gpt.py
+++ app_gpt.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
